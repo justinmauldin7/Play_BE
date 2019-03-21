5dd03cc20f5fa03209e74022b1c44b779b74fa9e
Justin Mauldin <justin.mauldin7@gmail.com>
2019-03-20 20:59:23 -0600

    Adds the module export of app & database in index.js file for the test file.

--- index.js
+++ index.js
@@ -100,3 +100,8 @@ app.get('/api/v1/playlists', (request, response) => {
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
+
+module.exports = {
+  app: app,
+  database: database
+}
