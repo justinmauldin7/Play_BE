c70171ed688ec8a4039afcb2c19d6bbf38926658
Justin Mauldin <justin.mauldin7@gmail.com>
2019-03-20 21:14:38 -0600

    Adds the truncation of the database to the routes test file.

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -7,30 +7,36 @@ const database = require('../index').database;
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-  before((done) => {
-      database.migrate.latest()
-        .then(() => done())
-        .catch(error => {
-          throw error;
-        });
-    });
+  before((done) => {[
+    database.raw("TRUNCATE playlists_favorites restart identity;")
+      .then(() => database.raw("TRUNCATE playlists restart identity CASCADE;"))
+      .then(() => database.raw("TRUNCATE favorites restart identity CASCADE;"))
+      .catch(error => {
+        throw error;
+    }),
+    database.migrate.latest()
+      .then(() => done())
+      .catch(error => {
+        throw error;
+    })
+  ]});
 
-    beforeEach((done) => {
-      database.seed.run()
-        .then(() => done())
-        .catch(error => {
-          throw error;
-        });
+  beforeEach((done) => {
+    database.seed.run()
+      .then(() => done())
+      .catch(error => {
+        throw error;
     });
+  });
 
   it('should return the homepage with text', done => {
-      chai.request(server)
-      .get('/')
-      .end((err, response) => {
-        response.should.have.status(200);
-        response.should.be.html;
-        response.res.text.should.equal('Hello, Players');
-        done();
-      });
+    chai.request(server)
+    .get('/')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.html;
+      response.res.text.should.equal('Hello, Players');
+      done();
     });
+  });
 });
