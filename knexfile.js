fe8dd8ce5c2df3d9697ad47e9efcac3b522e74da
Justin Mauldin <justin.mauldin7@gmail.com>
2019-03-23 17:08:31 -0600

    Adds production environment to knexfile.js for deployment to heroku.

--- knexfile.js
+++ knexfile.js
@@ -11,5 +11,17 @@ module.exports = {
         directory: './db/seeds/dev'
       },
       useNullAsDefault: true
+  },
+
+  production: {
+    client: 'pg',
+      connection: process.env.DATABASE_URL,
+      migrations: {
+        directory: './db/migrations'
+      },
+      seeds: {
+        directory: './db/seeds/dev'
+      },
+      useNullAsDefault: true
   }
 };
