bdb52c51fb65dbef8066759d9c212f946b01316d
Daniel Briechle <danbriechle@hotmail.com>
2019-03-26 13:15:26 -0600

    adds use playlist to index.js

--- index.js
+++ index.js
@@ -20,18 +20,7 @@ app.get('/', (request, response) => {
 
 app.use('/api/v1/favorites', favorites)
 
-// app.get('/api/v1/playlists', (request, response) => {
-//   database('playlists').select()
-//   .then((playlists) => {
-//     response.status(200).json(playlists)
-//   })
-//   .catch((error) => {
-//     response.status(500).json({error})
-//   });
-// });
-//
-// // I think the below statement will work but I'm not sure how to format it in the request
-// //select playlists.*, favorites.* from playlists, favorites;
+app.use('/api/v1/playlists', playlists)
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
